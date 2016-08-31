554c0fbd90eea7896e2d9525712fbb7ad906189b
Giacomo Torlai <torlaigiacomo@gmail.com>
2016-08-31 11:04:10 -0400

    Convolutional Psi Layer

---
+++ PsiConv.cpp
@@ -0,0 +1,78 @@
+#include "PsiConv.h"
+
+//*****************************************************************************
+// Constructor 
+//*****************************************************************************
+
+PsiLayer::PsiLayer(MTRand& random, int nIn, int nF, double B) 
+{
+    
+    n_in = nIn;
+    n_f = nF;
+    bound = B;
+
+    vector<double> temp;
+    double r;
+
+    for (int i=0; i<n_f; i++) {
+        for (int k=0; k<n_in; k++) {
+            r = random.rand();
+            temp.push_back(bound*(2.0 * r - 1.0));
+        }
+        Z.push_back(temp);
+        temp.clear();
+    }
+    c = 0.0;
+}
+
+
+//*****************************************************************************
+// Forward Pass 
+//*****************************************************************************
+
+double PsiLayer::forward_pass(vector<vector<double> > & input) {
+    
+    double activation= 0.0;
+    double psi;
+    
+    for (int k=0; k<n_in; k++) {
+        for (int i=0; i<n_f; i++) {
+            activation += Z[i][k] * input[k][i]; 
+        }
+    }
+    psi = 1.0/(1.0+exp(-activation));
+
+    return psi;
+}
+
+//*****************************************************************************
+// Save the Network Parameters
+//*****************************************************************************
+
+void PsiLayer::loadParameters(ofstream & file) 
+{
+        
+    for (int k=0; k<n_in; k++) {
+        for (int i=0; i<n_f; i++) {
+            file >> Z[i][k];
+        }
+    }
+}
+
+
+//*****************************************************************************
+// Save the Network Parameters
+//*****************************************************************************
+
+void PsiLayer::saveParameters(ofstream & file) 
+{
+
+    for (int k=0; k<n_in; k++) {
+        for (int i=0; i<n_f; i++) {
+            file << Z[i][k] << "  ";
+        }
+        file << endl;
+    }
+    file << endl << endl;
+}
+
---
+++ PsiConv.h
@@ -0,0 +1,40 @@
+#ifndef PSICONV_H
+#define PSICONV_H
+
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include "MersenneTwister.h"
+//#include <Eigen/Core>
+//#include <map>
+#include <complex>
+
+using namespace std;
+//using namespace Eigen;
+
+class PsiLayer {
+    
+    public:
+        
+        //Network Parameters
+        int n_f;        //Number of Filters of the previous hidden layer
+        int n_in;       //Number of possible configuration of filters
+        double bound;   //Initial width of weigth distribution
+        
+        vector<vector<double> > Z;      //Weigths
+        double c;                       //Biase
+
+        // Constructor
+        PsiLayer(MTRand & random, int nIn, int nF, double B);
+        
+        // Forward the input signal through the layer
+        double forward_pass_CONV(vector<vector<double> > & input);
+
+        //Parameters Saving and Loading
+        void loadParameters(ofstream & file);
+        void saveParameters(ofstream & file);
+
+        
+};
+
+#endif
